addd5f5a3b7e33b5c1dea738dca9b79858641b04
calebthebrewer <calebthebrewer@gmail.com>
2014-03-27 02:04:12 -0400

    beautifying tiles and adding endless scroll

--- js/app.js
+++ js/app.js
@@ -31,10 +31,10 @@ var api =  {
 			var append = path.indexOf("?") !== -1 ? "&" : "?",
 				token = SC.accessToken() ? "oauth_token=" + SC.accessToken() + "&" : "client_id=" + this.client_id + "&";
 			return "https://api.soundcloud.com" 
-					+ path + append + token + "format=json";
+					+ path + append + token + "format=json&order=hotness";
 		},
 		search: function(q, callback) {
-			$.getJSON(this.getUrl("/tracks?q=" + q + "&limit=10")).then(function(data) {
+			$.getJSON(this.getUrl("/tracks?q=" + q + "&limit=" + search.get('limit') + "&offset=" + search.get('offset'))).then(function(data) {
 				var normalized = [];
 				jQuery.each(data, function(index, obj) {
 					normalized.push({
@@ -42,8 +42,10 @@ var api =  {
 						title: obj.title,
 						url: obj.uri,
 						image: obj.artwork_url ? obj.artwork_url : '',
-						likes: obj.favoritings_count,
-						plays: obj.playback_count
+						likes: niceName.number(obj.favoritings_count),
+						plays: niceName.number(obj.playback_count),
+						comments: niceName.number(obj.comment_count),
+						ratio: Math.round((obj.favoritings_count / obj.playback_count) * 1000) / 10
 					});
 				});
 				callback(normalized);
@@ -72,20 +74,35 @@ var api =  {
 			return "https://www.googleapis.com/youtube/v3/" + path + "&key=" + this.client_id;
 		},
 		search: function(q, callback) {
-			$.getJSON(this.get_url("search?part=snippet&type=video&maxResults=10&q=" + q)).then(function(data) {
+			$.getJSON(this.get_url("search?part=id&type=video&videoEmbeddable=true" + 
+					"&safeSearch=none&order=rating&maxResults=" + search.get('limit') + "&offset=" + search.get('offset') + "&q=" + q)).then(function(data) {
+				//get videos
 				var normalized = [];
+				var ids = false;
 				jQuery.each(data.items, function(index, obj) {
-					var image = obj.snippet.thumbnails['default'].url ? 
-								obj.snippet.thumbnails['default'].url : '';
-					normalized.push({
-						service: 'youtube',
-						title: obj.snippet.title,
-						image: image,
-						description: obj.snippet.description,
-						id: obj.id.videoId
-					});
+					ids = ids ? ids + "," + obj.id.videoId : obj.id.videoId;
 				});
-				callback(normalized);
+				//get ratings for videos (this is seperate because google sucks)
+				$.getJSON(api.youtube.get_url("videos?part=snippet,statistics&id=" + ids)).then(function(data) {
+					$.each(data.items, function(index, obj) {
+						var image = obj.snippet.thumbnails['default'].url ? 
+							obj.snippet.thumbnails['default'].url : '';
+						normalized.push({
+							service: 'youtube',
+							title: obj.snippet.title,
+							image: image,
+							description: obj.snippet.description,
+							id: obj.id.videoId,
+							likes: niceName.number(obj.statistics.likeCount),
+							dislikes: niceName.number(obj.statistics.dislikeCount),
+							plays: niceName.number(obj.statistics.viewCount),
+							comments: niceName.number(obj.statistics.commentCount),
+							ratio: Math.round((obj.statistics.likeCount / obj.statistics.viewCount) * 1000) / 10
+						});		
+					});
+					//call it back
+					callback(normalized);
+				});
 			});
 		},
 		play: function(content) {
@@ -152,7 +169,7 @@ queue.on('play', function(e) {
 });
 
 queue.on('remove', function(e) {
-	this.get('queue').splice(e.node.parentNode.getAttribute('data-index'), 1);
+	this.get('queue').splice(e.original.target.parentNode.parentNode.getAttribute('data-index'), 1);
 });
 
 queue.on('save', function(e) {
@@ -189,12 +206,26 @@ function playContent(content) {
 	}
 }
 
+var niceName = {};
+niceName.number = function(number) {
+	//make sure we have a number
+	if (typeof number != "number") number = parseInt(number);
+	//small numbers look good
+	if (number < 1000) return number;
+	//lets do bigger numbers like this
+	if (number < 1000000) return Math.round((number / 1000) * 10) / 10 + "k";
+	//fuck it, everything else
+	return Math.round((number / 1000000) * 10) / 10 + "m";
+};
+
 search = new Ractive({
 	el: "searchContainer",
 	template: "#search",
 	data: {
 		query: "",
 		results: [],
+		offset: 0,
+		limit: 15,
 		message: "Search Soundcloud and Youtube at the same time. It's like wow."
 	}
 });
@@ -204,6 +235,7 @@ search.on('search', function(e) {
 	//setup result handlers
 	this.set('results', []);
 	this.set('message', "Let me check on that for you.");
+	this.set('offset', 0);
 	results = [];
 	calls = 2;
 	//make calls
@@ -211,28 +243,95 @@ search.on('search', function(e) {
 	api.youtube.search(this.get('query'), searchCallback);
 });
 
+function getMore() {
+	search.set('message', "Gimme a sec...");
+	search.set('offset', search.get('offset') + search.get('limit'));	
+	results = [];
+	calls = 2;
+	//make calls
+	api.soundcloud.search(search.get('query'), searchCallback);
+	api.youtube.search(search.get('query'), searchCallback);
+}
+
+$("#searchContainer").find(".content-panel-body-inner")[0].onscroll = function(ev) {
+	if (($("#searchContainer").find(".content-panel-body-inner")[0].scrollTop + $("#searchContainer").find(".content-panel-body-inner")[0].offsetHeight) 
+			>= $("#searchContainer").find(".content-panel-body-inner")[0].scrollHeight - 75 && !calls) {
+		getMore();
+	}
+};
+
 var results = [],
 	calls = 0;
 function searchCallback(data) {
 	results = jQuery.merge(results, data);
 	if (!--calls) {
-		results = shuffle(results);
-		search.set('results', results);
+		results.sort(function(a, b) {
+			return b.ratio > a.ratio;
+		});
+		search.set("message", "");
+		var allResults = search.get('results');
+		allResults = allResults.concat(results);
+		search.set('results', allResults);
 		if (!results.length) {
 			search.set('message', "Wow, you suck at this game. Try looking for something better.");
 		}
 	}
 }
 
-function shuffle(o) {
-	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
+queue.playLast = function() {
+	$(".queue").find("li:last").click();
 }
 
 search.on('addToQueue', function(e) {
+	//add this shit to the queue
 	var content = this.get('results')[e.node.getAttribute('data-index')];
 	queue.get('queue').push(content);
+	
+	//if nothing in queue, also play this shit
+	if (queue.get('queue').length == 1) {
+		queue.playLast();
+	}
+	//else gimme some kind of options
+	else {
+		toolTip.show(
+			e.original.clientX - 100, e.original.clientY - 20,
+			function() {	//play
+				queue.playLast();
+			}
+		);
+	}
 });
+
+/** Tool tip with me **/
+var toolTip = (function(){
+	var $shades = $("#tool-tip"),
+		$tip = $shades.find(".tip");
+		
+	//bind some things to our members
+	$shades.click(function(e) {
+		tt.hide();
+	});
+		
+	var tt = {};
+	tt.show = function(x, y, play, queue) {
+		$tip.css("left", x);
+		$tip.css("top", y);
+		$tip.find(".play").click(function(){
+			if (play) play();
+		});
+		$tip.find(".queue").click(function(){
+			if (queue) queue();
+		});
+		
+		$shades.show();
+	}
+	tt.hide = function() {
+		$shades.hide();
+	}
+	
+	return tt;
+})();
+//////////////////////
 
 services = new Ractive({
 	el: "servicesContainer",
@@ -287,7 +386,7 @@ $(".content-panel-pin").click(function() {
 		side = $panel.hasClass('left-panel') ? 'left' : 'right',
 		otherSide = side == 'left' ? 'right' : 'left';
 	//remove other pin, if it exists
-	removeFocus(otherSide);
+	//removeFocus(otherSide);
 	//add this pin
 	$(this).toggleClass('active');
 	$panel.toggleClass(side + '-panel-focus');
